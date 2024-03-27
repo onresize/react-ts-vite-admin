f9ed8291f1cf4bca9c6bda3246210f158534d052
Embrance-T <2069814988@qq.com>
2024-03-27 15:22:30 +0800

    style: 🎨 修改样式

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,26 +13,7 @@ const Home = (_props: any) => {
 				borderColor: themeStyle.borderColor,
 				background: themeStyle.bgColor
 			}}
-		>
-			<img
-				className="yun"
-				draggable="false"
-				src="https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*DR_FR7Q721sAAAAAAAAAAAAADmJ7AQ/original"
-				alt="#"
-			/>
-			<img
-				className="antV"
-				draggable="false"
-				src="https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*ZJscTJdtQqgAAAAAAAAAAAAADmJ7AQ/original"
-				alt="#"
-			/>
-			<img
-				className="worm"
-				draggable="false"
-				src="https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*Qua4RZ6djCcAAAAAAAAAAAAADmJ7AQ/original"
-				alt="#"
-			/>
-		</div>
+		></div>
 	);
 };
 
