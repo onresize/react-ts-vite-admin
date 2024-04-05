79db54ca6378a18c69ce564aecc73b438c454353
onresize <embrancelt@outlook.com>
2024-04-05 16:09:04 +0800

    fix: 🧩 update

--- .husky/format-code.sh
+++ .husky/format-code.sh
@@ -7,7 +7,7 @@ reset=$(tput sgr0)
 
 echo "》》》${green}开始按统一配置格式化暂存区代码...${reset}"
 
-if ! yarn lint-staged; then
+if ! yarn tsc && yarn lint-staged; then
     echo "《《《${red}格式化出错，请根据错误内容修改后再次尝试！${reset}"
     exit 1;
 fi
