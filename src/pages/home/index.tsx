bc00ff33b709cb83d07faafbec4ee9efcbe8acae
onresize <embrancelt@outlook.com>
2024-04-08 16:09:53 +0800

    fix: 🧩 update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,21 @@
 import React, { useContext, useState, useReducer, useRef, forwardRef, useImperativeHandle } from "react";
 import { ThemeContext } from "@/styles/theme/cssinJs";
 import { theme, Button } from "antd";
+import useStore from "@/mobx/index";
 // @ts-ignore
 import { home } from "./index.module.less";
 
 const Home = (_props: any) => {
 	const themeStyle: any = useContext(ThemeContext);
+	const { header } = useStore();
 
 	return (
 		<div
 			className={`${home} card`}
 			style={{
-				borderColor: themeStyle.borderColor,
-				background: themeStyle.bgColor
+				// background: themeStyle.bgColor,
+				border: header.themeType == "dark" ? `1px solid ${themeStyle.borderColor}` : "none",
+				filter: header.themeType == "light" ? "grayscale(100%) invert(1)" : ""
 			}}
 		></div>
 	);
